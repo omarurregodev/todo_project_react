498a599cd66910efc8ce8c117e9cc938772b55f4
omarurregodev <omarurrego07@gmail.com>
2022-07-21 01:59:57 -0500

    modificaciones de funcionalidad en el carrito, segun rubricas

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,6 +1,7 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect } from "react";
 import { cartContexto } from '../../Context/CartContext';
 import { Link } from 'react-router-dom';
+
 
 
 
@@ -9,6 +10,18 @@ const Cart = () => {
     const { productAdded } = useContext(cartContexto);
     const { clearCart } = useContext(cartContexto);
     const { removeProduct } = useContext(cartContexto);
+    
+    
+
+    
+    let totalPriceValue = 0;
+    
+    productAdded.forEach(products => {
+        totalPriceValue += products.quantity * products.price;
+    });
+   
+
+    
 
 
     return (
@@ -17,32 +30,43 @@ const Cart = () => {
                 <h3>Listado Carrito de Compras</h3>
                 <button className="waves-effect waves-light btn-small" onClick={clearCart}>Vaciar el Carrito</button>
             </div>
-            {productAdded.map((data) => {
-                return (
-                    <div  key={data.id} className="row valign-wrapper z-depth-3">
-                        <div className="col s1 m1 l1">
-                            <h4>ID: {data.id}</h4>
-                        </div>
-                        <div className="col s2 m2 l2">
-                            <img style={styles.imgCartItem} src={data.url} alt="" />
-                        </div>
-                        <div className="col s5 m5 l5">
-                            <div className="row">
-                                <h4>{data.name}</h4>
+            {
+                productAdded.length === 0
+                ? <><h3>No has agregado productos todavia.</h3>
+                    <Link to='/'>
+                        <button className="waves-effect waves-light btn-small orange darken-1">Ver Catalogo</button>
+                    </Link></>
+                : productAdded.map((data) => {
+                    return (
+                        <div  key={data.id} className="row valign-wrapper z-depth-3">
+                            <div className="col s1 m1 l1">
+                                <h4>ID: {data.id}</h4>
                             </div>
-                            <div className="row">
-                                <h5>Cantidad: {data.quantity}</h5>
+                            <div className="col s2 m2 l2">
+                                <img style={styles.imgCartItem} src={data.url} alt="" />
                             </div>
-                        </div>
-                        <div className="col s4 m4 l4" style={styles.btnCartItem}>
-                            <Link to={`/product/${data.id}`}>
-                                <button className="waves-effect waves-light btn-small blue lighten-1">Modificar</button>
-                            </Link>
-                            <button className="waves-effect waves-light btn-small red lighten-2" onClick={() => {removeProduct(data.id)}}>Eliminar</button>
-                        </div>
-                    </div> 
-                )
-            })}
+                            <div className="col s5 m5 l5">
+                                <div className="row">
+                                    <h4>{data.name}</h4>
+                                </div>
+                                <div className="row">
+                                    <h6>Cantidad: {data.quantity}</h6>
+                                    <h6>Precio total: $ {data.quantity * data.price}</h6>
+                                </div>
+                            </div>
+                            <div className="col s4 m4 l4" style={styles.btnCartItem}>
+                                <Link to={`/product/${data.id}`}>
+                                    <button className="waves-effect waves-light btn-small blue lighten-1">Modificar</button>
+                                </Link>
+                                <button className="waves-effect waves-light btn-small red lighten-2" onClick={() => {removeProduct(data.id)}}>Eliminar</button>
+                            </div>
+                        </div> 
+                    )
+                }
+            )}
+            <div className="row">
+                <h3>Precio total: $ {totalPriceValue}</h3>
+            </div>
         </div>
     )
 }
